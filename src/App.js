4c3a9420155323e76db813194d5db038d2b82b10
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2024-01-11 11:46:07 -0300

    Feat: add background color

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import "./App.css";
 import Header from "./Components/Header";
 import Homepage from "./Pages/Homepage";
 import CoinPage from "./Pages/CoinPage";
+import { makeStyles } from "@mui/material";
 
 function App() {
+  const useStyles = makeStyles(() => ({
+    App: {
+      backgroundColor: "#14161a",
+      color: "white",
+      minHeight: "100vh",
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
     <BrowserRouter>
-      <div>
+      <div className={classes.App}>
         <Header />
         <Route path="/" Component={Homepage} exact />
         <Route path="/coins/:id" Component={CoinPage} />
