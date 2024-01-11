0b2079fe164a74c800975f2178357f49a9f0143f
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2024-01-11 11:31:16 -0300

    Feat: adds mont serrat font and pagination structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header";
+import Homepage from "./Pages/Homepage";
+import CoinPage from "./Pages/CoinPage";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Route path="/" Component={Homepage} exact />
+        <Route path="/coins/:id" Component={CoinPage} />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
