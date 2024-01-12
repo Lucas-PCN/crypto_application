7b675ff3b08076c4d8ac2217ae7b6e30fd2faf29
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2024-01-12 18:29:26 -0300

    Fix: corrects Same Origin Policy error

--- src/Components/CoinsTable.js
+++ src/Components/CoinsTable.js
@@ -11,11 +11,28 @@ const CoinsTable = () => {
 
   const fetchCoins = async () => {
     setLoading(true);
-    const { data } = await axios.get(CoinList(currency));
 
-    setCoins(data);
-    setLoading(false);
+    const apiUrl = CoinList(1, 100, currency);
+
+    try {
+      // Adicione o prefixo do CORS Anywhere à URL da API
+      const proxyUrl = "https://cors-anywhere.herokuapp.com/" + apiUrl;
+
+      const { data } = await axios.get(proxyUrl, {
+        headers: {
+          "X-Requested-With": "XMLHttpRequest",
+        },
+      });
+
+      setCoins(data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Erro ao obter dados da API:", error);
+      setLoading(false);
+    }
   };
+
+  console.log(coins);
 
   useEffect(() => {
     fetchCoins();
